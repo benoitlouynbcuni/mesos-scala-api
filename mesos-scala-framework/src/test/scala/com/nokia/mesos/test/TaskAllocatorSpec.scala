6d251d2130388c2e1b126050a473a8795ae29069
Benoit Louy <benoit.louy@nbcuni.com>
2019-01-03 11:48:18 -0500

    fix test compilation errors

--- mesos-scala-framework/src/test/scala/com/nokia/mesos/test/TaskAllocatorSpec.scala
+++ mesos-scala-framework/src/test/scala/com/nokia/mesos/test/TaskAllocatorSpec.scala
@@ -39,8 +39,8 @@ class TaskAllocatorSpec extends FlatSpec with Matchers {
 
   val ra = Resource("a", Value.Type.SCALAR, Some(Value.Scalar(5)))
   val rb = Resource("b", Value.Type.SCALAR, Some(Value.Scalar(6)))
-  val td1 = TaskRequest(TaskDescriptor("task 1", immutable.Seq(ra), Left(CommandInfo())), TaskID("tid1"))
-  val td2 = TaskRequest(TaskDescriptor("task 2", immutable.Seq(rb), Left(CommandInfo())), TaskID("tid2"))
+  val td1 = TaskRequest(TaskDescriptor("task 1", immutable.Seq(ra), Some(CommandInfo())), TaskID("tid1"))
+  val td2 = TaskRequest(TaskDescriptor("task 2", immutable.Seq(rb), Some(CommandInfo())), TaskID("tid2"))
 
   "Allocator" should "work for anti-affinity" in {
     val tasks = Seq(td1, td2)
